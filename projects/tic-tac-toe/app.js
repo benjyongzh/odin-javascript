1c7a1ca5fd5aa4e8698bde236c9655d205f1b633
Benjamin Yong <benj_yongzh@hotmail.com>
2022-10-25 23:04:40 +0800

    Split init() into reset() and init() so that init() can take in fresh playerObjects.

--- projects/tic-tac-toe/app.js
+++ projects/tic-tac-toe/app.js
@@ -6,47 +6,9 @@ const gameBoard = (() => {
     const _boardComponent = document.querySelector(".gameboard");
     const _contentText = document.querySelector(".content-text");
     let _boardArray = [];
-    
-    const init = () => {
-        _players = [];
-        _playerTurn = 0;
-
-        //renew board tiles
-        _boardArray = [];
-        _boardComponent.replaceChildren();
-        for (let i = 0; i < 3; i++){
-            let boardRow = [];
-            for (let j = 0; j < 3; j++){
-                const boardTile = document.createElement("div");
-                boardTile.classList.add("boardTile");
-                boardTile.setAttribute('positionX', i);
-                boardTile.setAttribute('positionY', j);
-                boardTile.addEventListener('click', placeMark);
-                _boardComponent.appendChild(boardTile);
-                boardRow.push(boardTile);
-                //console.log(boardRow);
-            };
-            _boardArray.push(boardRow);
-            //console.log(boardArray)
-        };
-
-    };
 
     const showBoard = () => {
         console.log(_boardArray);
-    }
-
-    /* const getPlayerTurn = () => {
-        return _playerTurn;
-    } */
-
-    const _setNextPlayerTurn = () => {
-        if (_playerTurn >= _players.length-1) {
-            _playerTurn = 0;
-        } else {
-            _playerTurn += 1;
-        }
-        _contentText.textContent = `${_players[_playerTurn].name}'s turn...`;
     }
 
     const getPlayersAll = () => {
@@ -65,19 +27,60 @@ const gameBoard = (() => {
         _players.push(player);
     }
 
+    const _setNextPlayerTurn = () => {
+        if (_playerTurn >= _players.length-1) {
+            _playerTurn = 0;
+        } else {
+            _playerTurn += 1;
+        }
+        _contentText.textContent = `${getCurrentPlayer().name}'s turn...`;
+    }
+
     const setMark = (tile, mark) => {
         //use _boardArray, textContent and setAttribute
         tile.textContent = mark;
         tile.setAttribute("player-choice", mark);
         _setNextPlayerTurn();
         showBoard();
+        _checkEndgame();
     }
-    
+
+    const _checkEndgame = () => {
+
+    }
+
+    const reset = () => {
+        _players = [];
+        _playerTurn = 0;
+
+        //renew board tiles
+        _boardArray = [];
+        _boardComponent.replaceChildren();
+    }
+
+    const init = () => {
+        for (let i = 0; i < 3; i++){
+            let boardRow = [];
+            for (let j = 0; j < 3; j++){
+                const boardTile = document.createElement("div");
+                boardTile.classList.add("boardTile");
+                boardTile.setAttribute('positionX', i);
+                boardTile.setAttribute('positionY', j);
+                boardTile.addEventListener('click', placeMark);
+                _boardComponent.appendChild(boardTile);
+                boardRow.push(boardTile);
+            };
+            _boardArray.push(boardRow);
+        };
+
+        _contentText.textContent = `Welcome to Tic Tac Toe! ${getCurrentPlayer().name} shall start...`;
+
+    };
     
     return {
+        reset,
         init,
         showBoard,
-        // getPlayerTurn,
         getPlayersAll,
         getPlayerByIndex,
         getCurrentPlayer,
@@ -87,25 +90,11 @@ const gameBoard = (() => {
 
 })();
 
-const displayController = (() => {
-    
-    const render = () => {
-        gameBoard.showBoard();
-    }
-
-    
-    return {
-        render,
-    };
-
-})();
-
 function placeMark(event) {
-    const mark = gameBoard.getCurrentPlayer().sign;
-    
     //use data attribute to determine if chosen
     if (event.target.hasAttribute("player-choice")) return;
-    
+
+    const mark = gameBoard.getCurrentPlayer().sign;
     gameBoard.setMark(event.target, mark);
 };
 
@@ -115,9 +104,9 @@ const player = (name, sign) => {
     };
 };
 
-gameBoard.init();
+gameBoard.reset();
 const player1 = player("player 1", "X");
 const player2 = player("player 2", "O");
 gameBoard.addPlayer(player1);
 gameBoard.addPlayer(player2);
-
+gameBoard.init();
