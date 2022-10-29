a2d0e3239edcd0374bf3716598568ee162196aa1
Benjamin Yong <benj_yongzh@hotmail.com>
2022-10-29 22:09:12 +0800

    Asset Management tutorial in webpack.

--- exercises/es6/webpack-demo/src/index.js
+++ exercises/es6/webpack-demo/src/index.js
@@ -1,12 +1,25 @@
 // import _ from 'lodash';
-import myName from './myName';
+import {myName} from './myName';
+import './style.css';
+import Photo from './my-image.jpg';
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
 //   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-element.textContent = myName("Cody");
+  element.textContent = myName("Cody");
+  element.classList.add('hello');
+
+  const myImage = new Image();
+  myImage.src = Photo;
+
+  element.appendChild(myImage);
+
+  console.log(Data);
+  console.log(Notes);
 
   return element;
 }
