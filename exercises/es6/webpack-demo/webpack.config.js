ff3bbdf5360911e4155160b41672d4105762b0e0
Benjamin Yong <benj_yongzh@hotmail.com>
2022-10-30 11:30:11 +0800

    webpack --watch and dev-server and source mapping.

--- exercises/es6/webpack-demo/webpack.config.js
+++ exercises/es6/webpack-demo/webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: "./dist",
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  optimisation: {
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
