83e4049375b71312cab53a3a455b71ee55ca7cc5
Benjamin Yong <benj_yongzh@hotmail.com>
2022-11-01 21:36:46 +0800

    use .content for adding components.

--- projects/restaurant-page/src/index.js
+++ projects/restaurant-page/src/index.js
@@ -2,15 +2,6 @@ import './style.css';
 import headerComponent from './components/header.js';
 import menuContentComponent from './components/menu-content';
 
-/* function component() {
-    const element = document.createElement('.content');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.textContent = "Hello this is from index.js";
-  
-    return element;
-  } */
-
 const tabNames = ['Home', 'Contact', 'About'];
 const menuItems = {
   "Mains": {
@@ -24,5 +15,6 @@ const menuItems = {
   }
 };
 
-document.body.appendChild(headerComponent(tabNames));
-document.body.appendChild(menuContentComponent(menuItems));
+const main = document.querySelector(".content");
+main.appendChild(headerComponent(tabNames));
+main.appendChild(menuContentComponent(menuItems));
