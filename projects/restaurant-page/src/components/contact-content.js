fb3d5291f025666d5b3a9e42ea67aeaabbf9a6e4
Benjamin Yong <benj_yongzh@hotmail.com>
2022-11-02 22:04:51 +0800

    Add content for contact page. Add tab feature. index.js now works as an app object with a public init() method.

--- projects/restaurant-page/src/components/contact-content.js
+++ projects/restaurant-page/src/components/contact-content.js
@@ -3,11 +3,35 @@ export default function contactContentComponent(contactItems) {
     const main = document.createElement('div');
     main.classList.add("contact-content");
     // console.log(Object.keys(menuItems));
+
+    //title
+      const title = document.createElement('div');
+      title.classList.add('contact-title');
+      title.textContent = "Reach out to us!";
+      main.appendChild(title);
+
     
-    /* Object.entries(menuItems).forEach(entry => {
-      const [category, foodSet] = entry;
-      main.appendChild(menuCategoryComponent(category, foodSet));
-    }); */
+
+    Object.entries(contactItems).forEach(entry => {
+      const [category, text] = entry;
+      const infobox = document.createElement('div');
+      infobox.classList.add('contact-infobox');
+      infobox.setAttribute('id', category);
+
+      //category
+      const categoryInfo = document.createElement('div');
+      categoryInfo.classList.add('contact-info-category');
+      categoryInfo.textContent = category;
+      infobox.appendChild(categoryInfo);
+
+      //text
+      const textInfo = document.createElement('div');
+      textInfo.classList.add('contact-info-text');
+      textInfo.textContent = text;
+      infobox.appendChild(textInfo);
+
+      main.appendChild(infobox);
+    });
   
     return main;
   }
--- projects/restaurant-page/src/index.js
+++ projects/restaurant-page/src/index.js
@@ -4,54 +4,75 @@ import menuContentComponent from './components/menu-content';
 import contactContentComponent from './components/contact-content';
 import aboutContentComponent from './components/about-content';
 
-const tabNames = ['Home', 'Contact', 'About'];
-const menuItems = {
-  "Mains": {
-    "Aglio Olio": "Some delicious shit right here.",
-    "Chicken Rice": "Bro you can't go wrong with this standard dish.",
-    "Chicken Chop": "You wanna get dem gains or not?",
-  },
-  "Desserts": {
-    "Lava Cake": "Warm and cold at the same time. You love it.",
-    "Chendol": "Bad news: my wife isn't a fan. Good news: you're not my wife, so get this.",
+const app = (() => {
+
+  const tabNames = ['Home', 'Contact', 'About'];
+  const menuItems = {
+    "Mains": {
+      "Aglio Olio": "Some delicious shit right here.",
+      "Chicken Rice": "Bro you can't go wrong with this standard dish.",
+      "Chicken Chop": "You wanna get dem gains or not?",
+    },
+    "Desserts": {
+      "Lava Cake": "Warm and cold at the same time. You love it.",
+      "Chendol": "Bad news: my wife isn't a fan. Good news: you're not my wife, so get this.",
+    }
+  };
+  
+  const contactItems = {
+    "Who we are": "Get Fat Here",
+    "Where we are": "248 Henderson Road, #01-24, Singapore 589248",
+    "Call us at": "+65 68120552",
+  };
+  
+  const aboutItems = {
+    "title": "Who we are",
+    "text": "Have you ever wished you were fat? Tired of being on the side of society without fat privilege? Are you bullied for having a healthy and beautiful physique? Come on down to Get Fat Here, where you can... get fat here! No judgement on your looks and stature. We are all here to eat to our hearts content. We are open 24/7, because stomachs don't need a break.",
+  };
+  
+  const main = document.querySelector(".content");
+  
+  //menu
+  const menu = menuContentComponent(menuItems);
+  //main.appendChild(menu);
+  
+  //contact
+  const contact = contactContentComponent(contactItems);
+  //main.appendChild(contact);
+  
+  //about
+  const about = aboutContentComponent(aboutItems);
+  //main.appendChild(about);
+  
+  //header with tabs
+  const header = headerComponent(tabNames);
+  main.appendChild(header);
+  const menuTab = header.querySelector(`#${tabNames[0]}`);
+  menuTab.addEventListener('click', () => {
+    tabClicked(menu);
+  });
+  const ContactTab = header.querySelector(`#${tabNames[1]}`);
+  ContactTab.addEventListener('click', () => {
+    tabClicked(contact);
+  });
+  const AboutTab = header.querySelector(`#${tabNames[2]}`);
+  AboutTab.addEventListener('click', () => {
+    tabClicked(about);
+  });
+  
+  
+  function tabClicked(content) {
+    main.replaceChild(content, main.lastChild);
   }
-};
 
-const contactItems = {
-  "name": "Get Fat Here",
-  "address": "248 Henderson Road, #01-24, Singapore 589248",
-  "number": "+65 68120552",
-};
+  const init = () => {
+    main.appendChild(menu);
+  }
 
-const aboutItems = {
-  "title": "Who we are",
-  "text": "Have you ever wished you were fat? Tired of being on the side of society without fat privilege? Are you bullied for having a healthy and beautiful physique? Come on down to Get Fat Here, where you can... get fat here! No judgement on your looks and stature. We are all here to eat to our hearts content. We are open 24/7, because stomachs don't need a break.",
-};
+  return {
+    init
+  }
+})();
 
-const main = document.querySelector(".content");
 
-//header with tabs
-const header = headerComponent(tabNames);
-main.appendChild(header);
-const menuTab = header.querySelector(`#${tabNames[0]}`);
-menuTab.addEventListener('click', tabClicked);
-const ContactTab = header.querySelector(`#${tabNames[1]}`);
-ContactTab.addEventListener('click', tabClicked);
-const AboutTab = header.querySelector(`#${tabNames[2]}`);
-AboutTab.addEventListener('click', tabClicked);
-
-//menu
-const menu = menuContentComponent(menuItems);
-main.appendChild(menu);
-
-//contact
-const contact = contactContentComponent(contactItems);
-//main.appendChild(contact);
-
-//about
-const about = aboutContentComponent(aboutItems);
-//main.appendChild(about);
-
-function tabClicked(event) {
-  console.log(event);
-}
+app.init();
