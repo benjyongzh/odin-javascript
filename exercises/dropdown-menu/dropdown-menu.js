666490f7598aeede198b9b90dfc617ce0653078a
Benjamin Yong <benj_yongzh@hotmail.com>
2022-11-19 12:47:48 +0800

    Add linking.

--- exercises/dropdown-menu/dropdown-menu.js
+++ exercises/dropdown-menu/dropdown-menu.js
@@ -3,13 +3,37 @@
 //     }
 // }
 
-export default function dropdownMenu(options){
+export const dropdownMenu = (title, options={}) => {
+    const main = document.createElement('div');
+    main.classList.add('dropdown-main');
+
+    const link = createLink(options.mainLink="#", title);
+    main.appendChild(link);
+
     const menu = document.createElement('div');
-    menu.classList.add('dropdown-menu');
+    main.classList.add('dropdown-menu');
 
-    // const addSubmenu
+    main.appendChild(menu);
 
-    if (!options.appearOnHover) 
+    const showMenu = () => {
+        menu.classList.add('visible');
+    };
 
-    return menu;
+    let appearOnHover = options.appearOnHover ? options.appearOnHover : true;
+
+    main.addEventListener('click', {
+        if (!appearOnHover) {
+            showMenu();
+        };
+    });
+
+
+    return main;
+}
+
+const createLink = (link, text) => {
+    const anchor = document.createElement('a');
+    anchor.href = link;
+    anchor.textContent = text;
+    return anchor;
 }
---
+++ exercises/dropdown-menu/index.js
@@ -0,0 +1,4 @@
+import { dropdownMenu } from "./dropdown-menu.js";
+
+const menu = dropdownMenu({title: "hello", appearOnHover: true});
+document.appendChild(menu);
