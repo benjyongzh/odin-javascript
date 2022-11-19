b2440626960f20fa06a30137969bc7ecbeacee0c
Benjamin Yong <benj_yongzh@hotmail.com>
2022-11-19 11:57:20 +0800

    Create dropdown menu module.

---
+++ exercises/dropdown-menu/dropdown-menu.js
@@ -0,0 +1,15 @@
+// class dropdownMenu {
+//     constructor({appearOnHover, }){
+//     }
+// }
+
+export default function dropdownMenu(options){
+    const menu = document.createElement('div');
+    menu.classList.add('dropdown-menu');
+
+    // const addSubmenu
+
+    if (!options.appearOnHover) 
+
+    return menu;
+}
