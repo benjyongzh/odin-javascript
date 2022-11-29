f24e4918f2e7f21ab3d8bca2d6e291c8eef07702
Benjamin Yong <benj_yongzh@hotmail.com>
2022-11-29 22:00:15 +0800

    Add styling for images.

--- exercises/image-slider/src/index.js
+++ exercises/image-slider/src/index.js
@@ -1,11 +1,15 @@
 import "./style.css";
 import imageSliderComponent from "./image-slider";
+import wanaka from './img/wanaka-tree.jpg';
+import hookerValley from "./img/hooker-valley.jpg";
+import shipwreck from "./img/shipwreck.jpg";
+
 
 const body = document.querySelector("body");
 
 const carousel = imageSliderComponent();
-carousel.addImage("#thatwanakatree", "./src/img/wanaka-tree.jpg");
-carousel.addImage("Hooker Valley", "./src/img/hooker-valley.jpg");
-carousel.addImage("Shipwreck", "src/img/shipwreck.jpg");
+carousel.addImage("#thatwanakatree", wanaka);
+carousel.addImage("Hooker Valley", hookerValley);
+carousel.addImage("Shipwreck", shipwreck);
 
 body.appendChild(carousel.mainDOM);
--- exercises/image-slider/webpack.config.js
+++ exercises/image-slider/webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/i,
+                type: 'asset/resource',
             }
         ]
     }
