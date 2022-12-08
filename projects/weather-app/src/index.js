5d78712dd57bedb9e191b7fcc4c8e1ed5d272b4a
Benjamin Yong <benj_yongzh@hotmail.com>
2022-12-08 22:13:57 +0800

    Trying to solve the issue where the data is undefined, even after .json().

--- projects/weather-app/src/index.js
+++ projects/weather-app/src/index.js
@@ -5,14 +5,43 @@ const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=";
 const apiKey = "25ab02671e4a2079d81ab8a2c5b2c733";
 
 function constructFetchURL(url, location, key){
-    return url+location+"&appid="+key;
+    return url+location+"&appid="+key+"&units=metric";
 }
 
 async function getWeatherDataOfLocation(location){
-    const text = constructFetchURL(apiURL, location, apiKey);
-    console.log(text);
-    const response = await fetch(text, {mode: 'cors'});
-    console.log(response);
+    try {
+        const text = constructFetchURL(apiURL, location, apiKey);
+        // console.log(text);
+        const response = await fetch(text, {mode: 'cors'});
+        const info = await response.json();
+        //console.log(info);
+        return info;
+    } catch (error) {
+        console.log(error);
+    };
 };
 
-getWeatherDataOfLocation("london");
+async function extractData(arg){
+    try {
+/*         const name = data.name;
+        const weather = {
+            'type': data.weather,
+            'description': data.weather,
+            'temperature': data.main.temp,
+            'humidity': data.main.humidity,
+            'wind': data.wind,
+        };
+        return await Promise.all([name, weather]); */
+        //const name = await arg;
+        console.log(await arg);
+        console.log(await arg.name);
+
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+const data = getWeatherDataOfLocation("london");
+const extracted = extractData(data);
+console.log(extracted);
