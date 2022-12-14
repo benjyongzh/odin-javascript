20a12f02ab475367a187bcf5e3e2266048e9366b
Benjamin Yong <benj_yongzh@hotmail.com>
2022-12-14 22:20:01 +0800

    Complete replicate().

--- exercises/recursion/exercises.js
+++ exercises/recursion/exercises.js
@@ -129,3 +129,12 @@ console.log(sumSquares(l)); // 1 = 1
 
 l = [10,[[10],10],[10]] 
 console.log(sumSquares(l)); // 100 + 100 + 100 + 100 = 400 */
+
+function replicate(reps, num){
+    if (reps <= 0) return [];
+    return [num].concat(replicate(reps-1, num));
+};
+
+/* console.log(replicate(3, 5)) // [5, 5, 5]
+console.log(replicate(1, 69)) // [69]
+console.log(replicate(-2, 6)) // [] */
