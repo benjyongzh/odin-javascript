b02e42529ba24d14c65900829bc450030ccb773d
Benjamin Yong <benj_yongzh@hotmail.com>
2022-12-15 21:48:22 +0800

    Start on Linked List Project.

---
+++ projects/linked-lists/linked-list.js
@@ -0,0 +1,19 @@
+function linkedList(){
+    // append(value)
+    // prepend(value)
+    // size()
+    // head()
+    // tail()
+    // at(index)
+    // pop()
+    // contains(value)
+    // find(value)
+    // toString()
+    // insertAt(value,index)
+    // removeAt(index)
+};
+
+function node(value){
+    //value
+    //next
+};
