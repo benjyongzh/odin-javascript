739bf779e80b017cb2b749e5adf6ae76c32237a3
Benjamin Yong <benj_yongzh@hotmail.com>
2022-12-18 14:59:35 +0800

    Use bind() to put validity function into allVertices function.

--- projects/DSA/chess-graph/knights-travails.js
+++ projects/DSA/chess-graph/knights-travails.js
@@ -1,21 +1,99 @@
+//factory fn for a chess space
+const Vertex = data => {
+    let boardPosition = data;
+    let _adjacencyList = [];
 
-// create chess board using X and Y cartesian coordinates
-const createBoard = (horizontal,vertical) => {
-    let board = [];
-    for ( let i = 0; i < horizontal; i++){
-        let row = [];
-        for (let j = 0; j < vertical; j++){
-            row.push([i,j]);
-        };
-        board.push(row);
+    return {
+        get position() {return boardPosition},
+        get adjacencyList() {return _adjacencyList},
+        set adjacencyList(newList) {_adjacencyList = newList},
     };
-    return board;
 };
 
-const chessBoard = createBoard(8,8);
+// factory fn to create board using X and Y cartesian coordinates
+const Board = (horizontal,vertical) => {
 
-console.log(chessBoard);
+    let createBoard = (x, y) => {
+        let newBoard = [];
+        for ( let i = 0; i < horizontal; i++){
+            let row = [];
+            for (let j = 0; j < vertical; j++){
+                //push a chess space instead of a simple array
+                row.push(Vertex([i,j]));
+            };
+            newBoard.push(row);
+        };
+        return newBoard;
+    };
 
-// for each board space, create a list of other spaces are valid from that space, for a knight to traverse to. (adjacenccy list)
+    // for each board space, do something
+    let forAllVertices = callback => {
+        _array.forEach(row => {
+            row.forEach(vertex => {
+                // callback.bind(vertex);
+                // console.log("do smth here");
+                // callback.call(vertex, _array);
+                // callback(vertex.position);
+                callback({
+                    array: _array,
+                    vertex: vertex,
+                });
+            });
+        });
+    };
+
+    let _array = createBoard(horizontal,vertical);
+
+    return {
+        get array() {return _array},
+        forAllVertices,
+    };
+};
+
+
+
+// create a list of other spaces are valid from that vertex, for a knight to traverse to. (adjacenccy list)
+function createAdjacencyList(args){
+    let list = [];
+    // console.log(this);
+    args.array.forEach(row => {
+        row.forEach(space => {
+            if (this(args.vertex, space)) {
+                list.push([ space.position[0], space.position[1] ]);
+            };
+        });
+    });
+    args.vertex.adjacencyList = list;
+    console.log(args.vertex.adjacencyList);
+};
 
 // function to check for valid move from 1 space to another
+function checkValidKnightMove(pointA, pointB){
+    let x1 = pointA.position[0];
+    let y1 = pointA.position[1];
+    let x2 = pointB.position[0];
+    let y2 = pointB.position[1];
+    
+    let distanceX = Math.abs(x2 - x1);
+    let distanceY = Math.abs(y2 - y1);
+
+    //check x direction within range (value 1 or 2 only)
+    if (distanceX != 2 || distanceX != 1 ) return false;
+    //check y direction within range (value 1 or 2 only)
+    if (distanceY != 2 || distanceY != 1 ) return false;
+
+    console.log("not rejected yet");
+
+    // check for L-shape movements. (1,2) or (2,1) movements only
+    if ((distanceX == 2 && distanceY == 1) || 
+        (distanceX == 1 && distanceY == 2))
+        {
+        return true;
+    };
+    return false;
+};
+
+
+const chessBoard = Board(3,3);
+// console.log(chessBoard.array);
+chessBoard.forAllVertices(createAdjacencyList.bind(checkValidKnightMove));
