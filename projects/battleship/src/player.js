a9921e8205f97e398a5a38069bd16f1fac4c36ba
Benjamin Yong <benj_yongzh@hotmail.com>
2023-01-02 12:01:50 +0800

    Add functions for getRandomInt and selecting enemy players and boards.

--- projects/battleship/src/player.js
+++ projects/battleship/src/player.js
@@ -1,16 +1,38 @@
 import gameboard from "./gameboard";
 import game from "./gameManager";
 
-export default function player(boardSizeX = 7, boardSizeY = 7, isComputer = false){
+export default function player(boardSizeX = 8, boardSizeY = 8, isComputer = false){
 
     let _isComputer = isComputer;
+    let _targetGameboard;
+
+    function aimAtGameboard(board){
+        _targetGameboard = board;
+    }
 
     function attack(coordinates){
-
+        //fire receiveAttack on target gameboard
+        _targetGameboard.receiveAttack(coordinates[0], coordinates[1]);
     };
 
     function randomAttack(){
+        //select opponent
+        const enemyPlayer = getRandomPlayer({excludeCurrentPlayer: true});
 
+        //select opponent's gameboard
+        _targetGameboard = enemyPlayer.board;
+
+        //select a random point 
+        let pointX = getRandomInt(0,_targetGameboard.boardSize[0]);
+        let pointY = getRandomInt(0,_targetGameboard.boardSize[1]);
+
+        //make sure that random point is not recorded in its shotsTaken array
+        while (_targetGameboard.getSpace(pointX, pointY)){
+            pointX = getRandomInt(0,_targetGameboard.boardSize[0]);
+            pointY = getRandomInt(0,_targetGameboard.boardSize[1]);
+        }
+
+        attack([pointX, pointY]);
     };
 
     let _board = gameboard(boardSizeX, boardSizeY);
@@ -23,3 +45,20 @@ export default function player(boardSizeX = 7, boardSizeY = 7, isComputer = fals
         randomAttack,
     }
 };
+
+function getRandomPlayer(option = {excludeCurrentPlayer: true}){
+    let playerCount = game.players.length;
+    let randomIndex = getRandomInt(0,playerCount-1);
+    
+    if (option.excludeCurrentPlayer == true){
+        while(randomIndex !== game.players.indexOf(game.currentPlayer)){
+            randomIndex = getRandomInt(0,playerCount-1);
+        };
+    };
+    return game.players[randomIndex];
+}
+
+function getRandomInt(min,max){
+    let difference = max - min;
+    return min + (Math.floor(Math.random()*difference+1));
+}
