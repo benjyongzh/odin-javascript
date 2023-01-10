c8c40387d29584483dde09a6456e30e51cd21a31
Benjamin Yong <benj_yongzh@hotmail.com>
2023-01-10 23:57:00 +0800

    Convert Experience and Education Section Components into functional components.

--- projects/resume-builder/src/components/EducationSectionComponent.js
+++ projects/resume-builder/src/components/EducationSectionComponent.js
@@ -1,9 +1,54 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import InputComponent from "./InputComponent";
 import ContentButtonRemove from "./ContentButtonRemove";
 import "../styles/SubSectionComponent.css";
 
-class ExperienceSectionComponent extends Component{
+const EducationSectionComponent = props => {
+
+    const {institute, level, startYear, endYear, removeSection, sectionKey} = props;
+
+    const [showRemoveButton, setShowRemoveButton] = useState(false);
+
+    return(
+        <div className="subsection-container"  onMouseOver={() => setShowRemoveButton(true)} onMouseOut={() => setShowRemoveButton(false)}>
+
+            <ContentButtonRemove  visible={showRemoveButton} onButtonClick={removeSection} sectionKey={sectionKey} />
+
+            <InputComponent
+                id="institute"
+                textLabel="Institute"
+                inputType="text"
+                inputName="institute"
+                children={institute}
+            />
+            <InputComponent
+                id="level"
+                textLabel="Level"
+                inputType="text"
+                inputName="level"
+                children={level}
+            />
+            <InputComponent
+                id="startYear"
+                textLabel="Start Year"
+                inputType="number"
+                inputName="startYear"
+                children={startYear}
+            />
+            <InputComponent
+                id="endYear"
+                textLabel="End Year"
+                inputType="number"
+                inputName="endYear"
+                children={endYear}
+            />
+        </div>
+        
+    );
+};
+
+/* 
+class EducationSectionComponent extends Component{
     constructor(props){
         super(props);
         this.showRemoveButton = this.showRemoveButton.bind(this);
@@ -66,7 +111,7 @@ class ExperienceSectionComponent extends Component{
             
         );
     };
-};
+}; */
   
-export default ExperienceSectionComponent;
+export default EducationSectionComponent;
   
--- projects/resume-builder/src/components/ExperienceSectionComponent.js
+++ projects/resume-builder/src/components/ExperienceSectionComponent.js
@@ -1,9 +1,54 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import InputComponent from "./InputComponent";
 import ContentButtonRemove from "./ContentButtonRemove";
 import "../styles/SubSectionComponent.css";
 
-class ExperienceSectionComponent extends Component{
+const ExperienceSectionComponent = props => {
+
+    const {company, position, startYear, endYear, removeSection, sectionKey} = props;
+
+    const [showRemoveButton, setShowRemoveButton] = useState(false);
+
+    return(
+        <div className="subsection-container"  onMouseOver={() => setShowRemoveButton(true)} onMouseOut={() => setShowRemoveButton(false)}>
+
+            <ContentButtonRemove  visible={showRemoveButton} onButtonClick={removeSection} sectionKey={sectionKey} />
+
+            <InputComponent
+                id="company"
+                textLabel="Company"
+                inputType="text"
+                inputName="company"
+                children={company}
+            />
+            <InputComponent
+                id="position"
+                textLabel="Position"
+                inputType="text"
+                inputName="position"
+                children={position}
+            />
+            <InputComponent
+                id="startYear"
+                textLabel="Start Year"
+                inputType="number"
+                inputName="startYear"
+                children={startYear}
+            />
+            <InputComponent
+                id="endYear"
+                textLabel="End Year"
+                inputType="number"
+                inputName="endYear"
+                children={endYear}
+            />
+        </div>
+        
+    );
+};
+
+
+/* class ExperienceSectionComponent extends Component{
     constructor(props){
         super(props);
         this.showRemoveButton = this.showRemoveButton.bind(this);
@@ -66,7 +111,7 @@ class ExperienceSectionComponent extends Component{
             
         );
     };
-};
+}; */
   
 export default ExperienceSectionComponent;
   
