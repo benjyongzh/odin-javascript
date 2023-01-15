a4db44e6424133b423cad7f0e198de31b8b4f976
Benjamin Yong <benj_yongzh@hotmail.com>
2023-01-15 14:44:22 +0800

    Add pictures for cards. Performance is bad.

--- projects/memory-card/src/components/Card.js
+++ projects/memory-card/src/components/Card.js
@@ -1,14 +1,17 @@
 import React, {useState} from "react";
 import "../styles/Card.css";
+// import crab from "../img/crab.jpg";
 
 const Card = props => {
     const {cardName, onCardClick} = props;
 
     // const [selected, setSelected] = useState(false);
 
+    const imageSrc = require(`../img/${cardName.toLowerCase()}.jpg`);
+
     return(
         <div className="card" onClick={onCardClick}>
-            <div className="image"></div>
+            <img className="image" src={imageSrc}/>
             <div className="cardName">{cardName}</div>
         </div>
     );
