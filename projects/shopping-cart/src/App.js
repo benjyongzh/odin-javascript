cd4d0523d9d94ea8381d315b5718b4de92e68357
Benjamin Yong <benj_yongzh@hotmail.com>
2023-01-29 11:46:20 +0800

    Use App as the browserrouter. Add navbar with links.

--- projects/shopping-cart/src/App.js
+++ projects/shopping-cart/src/App.js
@@ -1,11 +1,20 @@
-import RouteSwitch from "./RouteSwitch";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+import Navbar from "./components/Navbar";
+import Tester from "./components/Tester";
+
+// import RouteSwitch from "./components/RouteSwitch";
 
 function App() {
   return (
-    <div className="App">
-      This is an app
-      <RouteSwitch/>
-    </div>
+    <BrowserRouter className="App">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- projects/shopping-cart/src/components/RouteSwitch.js
+++ projects/shopping-cart/src/components/RouteSwitch.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Tester from "./Tester";
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/Tester1" element={<Tester />} />
-        <Route path="/Tester2" element={<Tester />} />
+        <Route path="/" element={<Tester />} />
+        <Route path="/products" element={<Tester />} />
       </Routes>
     </BrowserRouter>
   );
