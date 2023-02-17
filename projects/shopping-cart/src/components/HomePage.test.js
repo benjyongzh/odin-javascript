4f5fbdae9415776c48e7369e776d8e6620778409
Benjamin Yong <benj_yongzh@hotmail.com>
2023-02-17 20:00:35 +0800

    Add Navbar.test.js.

--- projects/shopping-cart/src/components/HomePage.test.js
+++ projects/shopping-cart/src/components/HomePage.test.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { render, cleanup, screen } from "@testing-library/react";
-import HomePage from "./HomePage";
 import { BrowserRouter } from "react-router-dom";
 import "@testing-library/jest-dom";
+import HomePage from "./HomePage";
 
 afterEach(cleanup);
 
--- projects/shopping-cart/src/components/Navbar.test.js
+++ projects/shopping-cart/src/components/Navbar.test.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { render, cleanup, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
 import Navbar from "./Navbar";
 
 afterEach(cleanup);
 
-test.skip('check texts', () => {
-    render(<Navbar />);
-    expect(screen.getAllByRole('link').length).toStrictEqual(3);
+test('check texts', () => {
+    render(
+        <BrowserRouter>
+            <Navbar cartItems={{}} showCartState={jest.fn()} />
+        </BrowserRouter>
+    );
+    expect(screen.getAllByRole('link').length).toStrictEqual(2);
+    expect(screen.getByText('Home')).toBeInTheDocument();
+    expect(screen.getByText('Products')).toBeInTheDocument();
+    expect(screen.getByText('Cart')).toBeInTheDocument();
+    // console.log(screen.getAllByRole('link').length);
 });
