8d8c5440230e139048684b402f5c8bc94e79fb95
Benjamin Yong <benj_yongzh@hotmail.com>
2023-02-17 19:55:33 +0800

    Basic HomePage test works. Need to use BrowserRouter and import jest-dom.

--- projects/shopping-cart/src/components/HomePage.test.js
+++ projects/shopping-cart/src/components/HomePage.test.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { render, cleanup, screen } from "@testing-library/react";
 import HomePage from "./HomePage";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
 
 afterEach(cleanup);
 
 test('check texts', () => {
-    render(<HomePage />);
+    render(
+        <BrowserRouter>
+            <HomePage />
+        </BrowserRouter>
+    );
     expect(screen.getByText('Pok-e-Mart')).toBeInTheDocument();
 });
