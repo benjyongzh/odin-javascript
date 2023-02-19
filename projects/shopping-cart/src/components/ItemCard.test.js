40da7384ea1a592e73059d08b04539d917f0e89b
Benjamin Yong <benj_yongzh@hotmail.com>
2023-02-19 12:03:42 +0800

    Add boilerplate for testing. in testUtils.

--- projects/shopping-cart/src/components/ItemCard.test.js
+++ projects/shopping-cart/src/components/ItemCard.test.js
@@ -3,6 +3,7 @@ import { render, cleanup, screen, act, waitFor} from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import { createMemoryHistory } from "history";
 import "@testing-library/jest-dom";
+import { renderWithRouter } from "../testUtils";
 import userEvent from "@testing-library/user-event";
 
 import ItemCard from "./ItemCard";
@@ -14,29 +15,31 @@ afterEach(cleanup);
 const mockfn = jest.fn();
 jest.mock("../api");
 
-const renderCard = () => {
-    act(() => {
-        render(
-            <BrowserRouter>
-                <ItemCard itemName="mockItem" itemURL="#" addItemToCart={mockfn} />
-            </BrowserRouter>
-        );
-    });
-}
+// const renderCard = () => {
+//     act(() => {
+//         render(
+//             <BrowserRouter>
+//                 <ItemCard itemName="mockItem" itemURL="#" addItemToCart={mockfn} />
+//             </BrowserRouter>
+//         );
+//     });
+// }
 
 describe('item card', () => {
 
     it('has correct fixed texts', () => {
-        renderCard();
+        const {user} = renderWithRouter(<ItemCard />);
         expect(screen.getByRole('button')).toHaveTextContent('Quick Add');
         expect(screen.getByRole('link')).toHaveTextContent('Details');
         expect(screen.getAllByRole('img').length).toStrictEqual(1);
     });
 
-    it('Quick Add Button adds to cart', () => {
-        renderCard();
+    it.only('Quick Add Button adds to cart', () => {
+        const {user} = act(() => {
+            renderWithRouter(<ItemCard itemName="mockItem" itemURL="#" addItemToCart={mockfn} />);
+        })
         const button = screen.getByRole("button", { name: "Quick Add" });
-        userEvent.click(button);
+        user.click(button);
         expect(mockfn).toHaveBeenCalled();
     });
 
@@ -49,7 +52,7 @@ describe('item card', () => {
             image: "http://image.png",
         });
 
-        renderCard();
+        const {user} = renderWithRouter(<ItemCard itemName="mockItem" itemURL="#" addItemToCart={mockfn} />);
 
         await waitFor(() => {
             expect(screen.getByText("mockItemName")).toBeInTheDocument();
@@ -61,18 +64,36 @@ describe('item card', () => {
 
     it('link points to correct product id', async () => {
         api.mockResolvedValue({
+            name: "mockItemName",
             id: 27,
+            cost: 723,
+            description: "mock description",
+            image: "http://image.png",
         });
-        const history = createMemoryHistory();
-        history.push = jest.fn();
 
-        renderCard();
+        const history = createMemoryHistory(/* { initialEntries: ['/mock/route'] } */);
+        // history.push = jest.fn();
+        // const user = userEvent.setup();
+
+        // renderCard();
+        
+        render(
+            <BrowserRouter history={history}>
+                <ItemCard itemName="mockItem" itemURL="#" addItemToCart={mockfn} />
+            </BrowserRouter>
+        );
+
+        // expect(history.location.pathname).toEqual('/mock/route');
 
         const button = screen.getByRole("link", { name: "Details" });
-        act(() => {userEvent.click(button)});
+        // act(() => {
+            await userEvent.click(button);
+        // });
 
         await waitFor(() => {
-            expect(history.push).toHaveBeenCalledWith(`/products/27`);
+            // expect(history.location.pathname).toEqual('/products/27');
+            expect(screen.getByText("mock description")).toBeInTheDocument();
         });
+
     });
 });
