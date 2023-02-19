10399205fc138ec07934d5d54053f499b5951ef7
Benjamin Yong <benj_yongzh@hotmail.com>
2023-02-19 15:23:12 +0800

    Finish ItemCard tests.

--- projects/shopping-cart/src/components/ItemCard.test.js
+++ projects/shopping-cart/src/components/ItemCard.test.js
@@ -1,57 +1,53 @@
 import React from "react";
 import { render, cleanup, screen, act, waitFor} from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
 import { createMemoryHistory } from "history";
 import "@testing-library/jest-dom";
-import { renderWithRouter } from "../testUtils";
-import userEvent from "@testing-library/user-event";
-
-import ItemCard from "./ItemCard";
 
 import { fetchItemInfo as api } from "../api";
+import { renderWithRouter } from "../testUtils";
+import ItemCard from "./ItemCard";
 
-afterEach(cleanup);
-
+//mocks
 const mockfn = jest.fn();
 jest.mock("../api");
 
-// const renderCard = () => {
-//     act(() => {
-//         render(
-//             <BrowserRouter>
-//                 <ItemCard itemName="mockItem" itemURL="#" addItemToCart={mockfn} />
-//             </BrowserRouter>
-//         );
-//     });
-// }
+//setup
+beforeEach(() => {
+    api.mockResolvedValue({
+        name: "mockItemName",
+        id: 27,
+        cost: 723,
+        description: "mock description",
+        image: "http://image.png",
+    });
+})
 
+afterEach(cleanup);
+
+//tests
 describe('item card', () => {
 
-    it('has correct fixed texts', () => {
+    it('has correct fixed texts', async () => {
         const {user} = renderWithRouter(<ItemCard />);
-        expect(screen.getByRole('button')).toHaveTextContent('Quick Add');
-        expect(screen.getByRole('link')).toHaveTextContent('Details');
-        expect(screen.getAllByRole('img').length).toStrictEqual(1);
+        await waitFor(() => {
+            expect(screen.getByRole('button')).toHaveTextContent('Quick Add');
+            expect(screen.getByRole('link')).toHaveTextContent('Details');
+            expect(screen.getAllByRole('img').length).toStrictEqual(1);
+        });
     });
 
-    it.only('Quick Add Button adds to cart', () => {
-        const {user} = act(() => {
-            renderWithRouter(<ItemCard itemName="mockItem" itemURL="#" addItemToCart={mockfn} />);
-        })
+    it('Quick Add Button adds to cart', async () => {
+        const {user} = renderWithRouter(<ItemCard itemName="mockItem" itemURL="#" addItemToCart={mockfn} />);
         const button = screen.getByRole("button", { name: "Quick Add" });
+
         user.click(button);
-        expect(mockfn).toHaveBeenCalled();
+
+        await waitFor(() => {
+            expect(mockfn).toHaveBeenCalled();
+        });
     });
 
     it('displays fetched data', async () => {
-        api.mockResolvedValue({
-            name: "mockItemName",
-            id: 27,
-            cost: 723,
-            description: "mock description",
-            image: "http://image.png",
-        });
-
         const {user} = renderWithRouter(<ItemCard itemName="mockItem" itemURL="#" addItemToCart={mockfn} />);
 
         await waitFor(() => {
@@ -63,36 +59,10 @@ describe('item card', () => {
     });
 
     it('link points to correct product id', async () => {
-        api.mockResolvedValue({
-            name: "mockItemName",
-            id: 27,
-            cost: 723,
-            description: "mock description",
-            image: "http://image.png",
-        });
-
-        const history = createMemoryHistory(/* { initialEntries: ['/mock/route'] } */);
-        // history.push = jest.fn();
-        // const user = userEvent.setup();
-
-        // renderCard();
-        
-        render(
-            <BrowserRouter history={history}>
-                <ItemCard itemName="mockItem" itemURL="#" addItemToCart={mockfn} />
-            </BrowserRouter>
-        );
-
-        // expect(history.location.pathname).toEqual('/mock/route');
-
+        const {user} = renderWithRouter(<ItemCard itemName="mockItem" itemURL="#" addItemToCart={mockfn} />);
         const button = screen.getByRole("link", { name: "Details" });
-        // act(() => {
-            await userEvent.click(button);
-        // });
-
         await waitFor(() => {
-            // expect(history.location.pathname).toEqual('/products/27');
-            expect(screen.getByText("mock description")).toBeInTheDocument();
+            expect(button).toHaveAttribute('href', '/products/27')
         });
 
     });
