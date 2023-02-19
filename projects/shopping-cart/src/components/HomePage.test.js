e349b0f4a812e8ca121e87763385d26a2dec9135
Benjamin Yong <benj_yongzh@hotmail.com>
2023-02-19 15:28:50 +0800

    Finish HomePage tests.

--- projects/shopping-cart/src/components/HomePage.test.js
+++ projects/shopping-cart/src/components/HomePage.test.js
@@ -1,27 +1,31 @@
 import React from "react";
-import { render, cleanup, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
+import { cleanup, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+
+import { renderWithRouter } from "../testUtils";
 import HomePage from "./HomePage";
 
-afterEach(cleanup);
-
+//mocks
 const mockfn = jest.fn();
 
-const renderHomePage = () => {
-    render(
-        <BrowserRouter>
-            <HomePage />
-        </BrowserRouter>
-    );
-}
+//setup
+afterEach(cleanup);
 
+//tests
 describe('Home Page', () => {
     it('renders correct texts', () => {
-        renderHomePage();
+        const {user} = renderWithRouter(<HomePage />);
         expect(screen.getByText('Pok-e-Mart')).toBeInTheDocument();
         expect(screen.getByRole('link')).toHaveTextContent('Store');
         expect(screen.getAllByRole('img').length).toStrictEqual(1);
     })
-    
+
+    it('link points to correct product URL', () => {
+        const {user} = renderWithRouter(<HomePage />);
+        const button = screen.getByRole("link", { name: "Store" });
+        // await waitFor(() => {
+            expect(button).toHaveAttribute('href', '/products')
+        // });
+
+    });
 });
