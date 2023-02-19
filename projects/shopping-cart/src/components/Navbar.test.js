162f86b3048493da8ab0ff88f3bcc42386e36968
Benjamin Yong <benj_yongzh@hotmail.com>
2023-02-19 16:41:35 +0800

    Finish navbar tests.

--- projects/shopping-cart/src/components/Navbar.test.js
+++ projects/shopping-cart/src/components/Navbar.test.js
@@ -14,15 +14,24 @@ afterEach(cleanup);
 //tests
 describe('Navbar', () => {
     it('renders correct texts', () => {
-        const {user} = renderWithRouter(<Navbar cartItems={{}} showCartState={mockfn} />);
+        const {user} = renderWithRouter(<Navbar cartItems={[]} showCartState={mockfn} />);
         expect(screen.getAllByRole('link').length).toStrictEqual(2);
         expect(screen.getByText('Home')).toBeInTheDocument();
         expect(screen.getByText('Products')).toBeInTheDocument();
         expect(screen.getByText('Cart')).toBeInTheDocument();
     });
 
+    it('links have correct hrefs', () => {
+        const {user} = renderWithRouter(<Navbar cartItems={[]} showCartState={mockfn} />);
+        const homeLink = screen.getByRole('link', {name: "Home"});
+        expect(homeLink).toHaveAttribute('href', '/')
+        const productsLink = screen.getByRole('link', {name: "Products"});
+        expect(productsLink).toHaveAttribute('href', '/products')
+    });
+
+
     it('toggles showCart()', async () => {
-        const {user} = renderWithRouter(<Navbar cartItems={{}} showCartState={mockfn} />);
+        const {user} = renderWithRouter(<Navbar cartItems={[]} showCartState={mockfn} />);
         const button = screen.getByRole('button');
         act(() => {
             user.click(button)
@@ -30,6 +39,15 @@ describe('Navbar', () => {
 
         await waitFor(() => {
             expect(mockfn).toHaveBeenCalled();
+        });
+    });
+
+    it('renders correct cart item quantity', async () => {
+        const {user} = renderWithRouter(<Navbar cartItems={["item 1", "item 2", "item 3"]} showCartState={mockfn} />);
+        const button = screen.getByRole('button');
+
+        await waitFor(() => {
+            expect(button).toHaveTextContent("Cart (3)");
         });
     });
 });
